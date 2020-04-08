99712bed53858b99db9a127db229de3fbe2e4fc5
王瑞瑜 <roywang@h2o-team.com>
2020-04-08 14:45:11 +0800

    add docs folder for deploy

---
+++ docs/precache-manifest.7c2130b831c6967f87f83c7c8ed54499.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5b1caf547e7de1701ba90224aeb343b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c23fce44e9cbfa53953f",
+    "url": "/static/css/main.76787685.chunk.css"
+  },
+  {
+    "revision": "f61992d9adca531c6fb1",
+    "url": "/static/js/2.168c7765.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.168c7765.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c23fce44e9cbfa53953f",
+    "url": "/static/js/main.20092cfd.chunk.js"
+  },
+  {
+    "revision": "7fa7ec6c853e3f22e43b",
+    "url": "/static/js/runtime-main.07aae395.js"
+  }
+]);

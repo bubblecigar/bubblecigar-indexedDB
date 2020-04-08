d55dccaf3c066f9ce91dac04d6f5a12950e19e63
王瑞瑜 <roywang@h2o-team.com>
2020-04-08 15:01:59 +0800

    Updates

---
+++ precache-manifest.ddcb70afda5fff4311031ccc6737a5fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24917e45561f7b8ffb24a567aec79f15",
+    "url": "/indexedDB/index.html"
+  },
+  {
+    "revision": "76d11f4df6d3b7aa3440",
+    "url": "/indexedDB/static/css/main.76787685.chunk.css"
+  },
+  {
+    "revision": "760f48c8f9fdfc427a8e",
+    "url": "/indexedDB/static/js/2.ad17f0af.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/indexedDB/static/js/2.ad17f0af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76d11f4df6d3b7aa3440",
+    "url": "/indexedDB/static/js/main.43fd819f.chunk.js"
+  },
+  {
+    "revision": "acc0b9b31c535d09b09f",
+    "url": "/indexedDB/static/js/runtime-main.ca57dbcc.js"
+  }
+]);
